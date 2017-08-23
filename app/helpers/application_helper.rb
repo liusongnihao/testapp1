d9764426db2f31d58631e59955a0258f7a1c82c4
liusong <819275067@qq.com>
2017-08-23 14:53:15 +0800

    FInish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def full_title(page_title ='')
+		base_title="Ruby ON Rails Tutorial Sample App"
+		if  page_title.empty?
+			base_title
+		else
+			page_title + " | " +base_title
+		end
+	end
+
+
 end
