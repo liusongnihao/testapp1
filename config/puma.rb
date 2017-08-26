7c2c5a9b3636c2a33647142cb8b6e86d0faa99d9
liusong <819275067@qq.com>
2017-08-26 12:21:38 +0800

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-workers Integer(ENV['WEB_CONCURRENCY'] ||2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV("RAILS_MAX_THREADS") ||  5 )
 threads threads_count, threads_count
 
@@ -12,11 +12,12 @@ threads threads_count, threads_count
 #
 preload_app!
 
+rackup   DefaultRackup
 port        ENV["PORT"] ||  3000
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV["RAILS_ENV"] || "development" 
+environment ENV["RACK_ENV"] || "development" 
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
