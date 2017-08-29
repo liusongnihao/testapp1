10185a781bfadea43f94b7c8c37c77f5f1d10fe3
liusong <819275067@qq.com>
2017-08-29 09:59:03 +0800

    implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -44,6 +44,8 @@ test "login with valid information followed by logout" do
 	delete logout_path
 	assert_not is_logged_in?
 	assert_redirected_to root_url
+	
+	delete logout_path
 	follow_redirect!
 	assert_select "a[href=?]",login_path
 	assert_select "a[href=?]",logout_path, count:0
@@ -51,6 +53,18 @@ test "login with valid information followed by logout" do
 
 end
 
+test "login with remembering" do 
+	log_in_as(@user,remember_me:'1')
+	assert_not_empty cookies['remember_token']
 
+end
+
+test "login without remembering" do
+	log_in_as(@user,remember_me:'1')
+	delete logout_path
+	log_in_as(@user,remember_me:'0')
+	assert_empty cookies['remember_token']
+
+end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,6 +76,10 @@ class UserTest < ActiveSupport::TestCase
 	
 
 	end
+	
+	test "authenticated? should return false for a user with nil digest" do
+		assert_not @user.authenticated?('')
+	end
 		
 
 
